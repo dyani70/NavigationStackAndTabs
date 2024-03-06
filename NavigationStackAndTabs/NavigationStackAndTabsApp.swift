568bf7aad8b0428491f0fb522d3a734ed964eb8d
Dyani <dyani70@gmail.com>
2024-03-06 23:57:48 +0900

    Initial Commit

---
+++ NavigationStackAndTabs/NavigationStackAndTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAndTabsApp.swift
+//  NavigationStackAndTabs
+//
+//  Created by Dyani on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAndTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
